950ce9f203b94c77497b1f8ada3970165c765fb3
Gois <guijocargo@gmail.com>
2017-09-26 12:31:50 -0300

    Remove out-tsc folder and add it to gitignore

--- out-tsc/lib-es2015/index.js
+++
@@ -1,6 +0,0 @@
-export { PriceFormatComponent } from './src/component/price-format.component';
-export { PriceFormatKeyEventChecker } from './src/utils/price-format.key-event-checker';
-export { PriceFormatOptions } from './src/utils/price-format.options';
-export { PriceFormatTransformer } from './src/utils/price-format.transformer';
-export { PriceFormatValidator } from './src/validator/price-format.validator';
-export { PriceFormatModule } from './src/module';
